292b848cdbbddc4c89c0be384043dff8574a9354
JP-code33 <pateljhenil@gmail.com>
2026-08-05 20:49:39 -0400

    completed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import filedialog
+from tkinter import filedialog, messagebox
 from reportlab.pdfgen import canvas
 from reportlab.lib.colors import white
 from PIL import Image
@@ -31,12 +31,19 @@ class ImageToPDFConverter:
         pdf_name_entry = tk.Entry(self.root,textvariable=self.output_pdf_name,width=35,justify='center',font=("Comic Sans MS", 11),bg="#252526",fg="white",insertbackground="white",bd=1,relief="solid")
         pdf_name_entry.pack(pady=(0,25))
 
-        convertButton = tk.Button(self.root,text="Convert to PDF",font=("Comic Sans MS",12,"bold"),fg="white",bg="#003b63",activebackground="#005999",activeforeground="white",padx=20,pady=8,command=self.convert_images_to_pdf)
-        convertButton.pack(pady=(0,25))
+        self.convertButton = tk.Button(self.root,text="Convert to PDF",font=("Comic Sans MS",12,"bold"),fg="white",bg="#003b63",activebackground="#005999",activeforeground="white",padx=20,pady=8,command=self.convert_images_to_pdf,state="disabled")
+        self.convertButton.pack(pady=(0,25))
+
+        footer = tk.Label(self.root,text="Supports PNG, JPG, JPEG\nMADE BY JP", font=("Comic Sans MS",9,"bold"),fg="#FFFFFF",bg="black")
+        footer.pack(side="bottom",pady=15)
         
     def select_images(self):
         self.image_paths = filedialog.askopenfilenames(title="Select Images", filetypes=[("Images files", "*.png;*.jpg;*.jpeg")])
         self.updateSelectedImageListbox()
+        if self.image_paths:
+            self.convertButton.config(state="normal")
+        else:
+            self.convertButton.config(state="disabled")
 
     def updateSelectedImageListbox(self):
         self.selected_images_listbox.delete(0,tk.END)
@@ -46,30 +53,38 @@ class ImageToPDFConverter:
 
     def convert_images_to_pdf(self):
         if not self.image_paths:
+            messagebox.showwarning("No Images Selected", "Please Select One or More Image(s)")
             return
 
         defaultName = self.output_pdf_name.get() if self.output_pdf_name.get() else "output"
-        output_pdf_path = filedialog.asksaveasfile(title="Save PDF As",initialfile=defaultName + ".pdf",defaultextension=".pdf",filetypes=[("PDF files", "*.pdf")])
-        if not self.output_pdf_path:
+        output_pdf_path = filedialog.asksaveasfilename(title="Save PDF As",initialfile=defaultName + ".pdf",defaultextension=".pdf",filetypes=[("PDF files", "*.pdf")])
+        if not output_pdf_path:
+            messagebox.showinfo("Cancelled","PDF conversion was cancelled")
             return
+        
+        try:
+            pdf = canvas.Canvas(output_pdf_path, pagesize=(612,792))
+            for image_path in self.image_paths:
+                img = Image.open(image_path)
+                availableWidth = 540
+                availableHeight = 720
+                scaleFactor = min(availableWidth / img.width, availableHeight / img.height)
+                newWidth = img.width * scaleFactor
+                newHeight = img.height * scaleFactor
+                x_centered = (612 - newWidth) / 2
+                y_centered = (792 - newHeight) / 2
 
-        pdf = canvas.Canvas(output_pdf_path, pagesize=(612,792))
-        for image_path in self.image_paths:
-            img = Image.open(image_path)
-            availableWidth = 540
-            availableHeight = 720
-            scaleFactor = min(availableWidth / img.width, availableHeight / img.height)
-            newWidth = img.width * scaleFactor
-            newHeight = img.height * scaleFactor
-            x_centered = (612 - newWidth) / 2
-            y_centered = (792 - newHeight) / 2
+                pdf.setFillColor(white)
+                pdf.rect(0,0,612,792,fill=True)
+                pdf.drawInlineImage(img, x_centered, y_centered, width=newWidth, height=newHeight)
+                pdf.showPage()
 
-            pdf.setFillColor(white)
-            pdf.rect(0,0,612,792,fill=True)
-            pdf.drawInlineImage(img, x_centered, y_centered, width=newWidth, height=newHeight)
-            pdf.showPage()
+            pdf.save()
+            messagebox.showinfo("Success",f"The PDF has been created and is saved to:\n{output_pdf_path}")
 
-        pdf.save()
+        except Exception as e:
+            messagebox.showerror("Conversion Failed",f"An error has occured while converting.\n{e}")
+
 
 
 
